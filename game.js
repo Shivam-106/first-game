f7ddc022abd1dfa9449636c725b7212d61bb0d95
Shivam-106 <69721189+Shivam-106@users.noreply.github.com>
2021-07-02 10:00:26 +0530

    start the game

--- game.js
+++ game.js
@@ -15,3 +15,11 @@ class mainScene {
       // It will handle all the game's logic, like movements
     }
   }
+  new Phaser.Game({
+    width: 700, // Width of the game in pixels
+    height: 400, // Height of the game in pixels
+    backgroundColor: '#3498db', // The background color (blue)
+    scene: mainScene, // The name of the scene we created
+    physics: { default: 'arcade' }, // The physics engine to use
+    parent: 'game', // Create the game inside the <div id="game"> 
+  });
